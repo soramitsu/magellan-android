8366183fa182780aa751309d4a9a0c23087a1805
Dmitry Selivanov <diselivanov@gmail.com>
2020-08-11 19:36:57 +0300

    BP-732: Update maps screen
    * Changed category icon size
    * Updated category name text style

--- library/src/main/java/jp/co/soramitsu/map/model/Category.kt
+++ library/src/main/java/jp/co/soramitsu/map/model/Category.kt
@@ -5,13 +5,13 @@ data class Category(
     val name: String
 ) {
     companion object {
-        val BANK = Category(-1, "bank")
-        val FOOD = Category(-2, "food")
-        val SERVICES = Category(-3, "services")
-        val SUPERMARKETS = Category(-4, "supermarkets")
-        val PHARMACY = Category(-5, "pharmacy")
-        val ENTERTAINMENT = Category(-6, "entertainment")
-        val EDUCATION = Category(-7, "education")
-        val OTHER = Category(-8, "other")
+        val BANK = Category(-1, "Bank")
+        val FOOD = Category(-2, "Food")
+        val SERVICES = Category(-3, "Services")
+        val SUPERMARKETS = Category(-4, "Supermarkets")
+        val PHARMACY = Category(-5, "Pharmacy")
+        val ENTERTAINMENT = Category(-6, "Entertainment")
+        val EDUCATION = Category(-7, "Education")
+        val OTHER = Category(-8, "Other")
     }
 }
