4419fa572be3aac50c49690e8cedb1dbdfad1cc1
Dmitry Selivanov <diselivanov@gmail.com>
2021-01-21 18:49:21 +0300

    BP-1002: Place proposal
    * Added category selection screen for proposal place

---
+++ library/src/main/java/jp/co/soramitsu/map/presentation/places/add/CategoriesAdapter.kt
@@ -0,0 +1,68 @@
+package jp.co.soramitsu.map.presentation.places.add
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import jp.co.soramitsu.map.R
+import jp.co.soramitsu.map.model.Category
+import jp.co.soramitsu.map.presentation.categories.CategoryTextView
+
+internal class CategoriesAdapter : RecyclerView.Adapter<CategoriesAdapter.CategoryViewHolder>() {
+
+    private val items = mutableListOf<Category>()
+    private var onCategorySelected: (Category) -> Unit = {}
+
+    fun setOnCategorySelectedListener(onCategorySelectedListener: (Category) -> Unit) {
+        onCategorySelected = onCategorySelectedListener
+    }
+
+    fun setItems(categories: List<Category>) {
+        val diffResult = DiffUtil.calculateDiff(CategoriesDiffUtilCallback(items, categories))
+        items.clear()
+        items.addAll(categories)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.sm_category_list_item, parent, false)
+        return CategoryViewHolder(view).also { viewHolder ->
+            view.setOnClickListener {
+                onCategorySelected.invoke(items[viewHolder.adapterPosition])
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    internal class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val categoryTextView: CategoryTextView = itemView.findViewById(R.id.categoryTextView)
+        fun bind(category: Category) {
+            categoryTextView.category = category
+        }
+    }
+
+    private class CategoriesDiffUtilCallback(
+        private val beforeList: List<Category>,
+        private val afterList: List<Category>
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize(): Int = beforeList.size
+        override fun getNewListSize(): Int = afterList.size
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return beforeList[oldItemPosition].id == afterList[newItemPosition].id
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return beforeList[oldItemPosition] == afterList[newItemPosition]
+        }
+
+    }
+}
--- library/src/main/java/jp/co/soramitsu/map/presentation/places/add/PlaceProposalFragment.kt
+++ library/src/main/java/jp/co/soramitsu/map/presentation/places/add/PlaceProposalFragment.kt
@@ -1,8 +1,42 @@
 package jp.co.soramitsu.map.presentation.places.add
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.model.LatLng
 import jp.co.soramitsu.map.R
+import jp.co.soramitsu.map.model.Category
+import kotlinx.android.synthetic.main.sm_fragment_place_proposal.*
 
-class PlaceProposalFragment : Fragment(R.layout.sm_fragment_place_proposal) {
+class PlaceProposalFragment : Fragment(R.layout.sm_fragment_place_proposal),
+    SelectPlaceCategoryFragment.OnCategorySelected {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbar.setNavigationOnClickListener { activity?.onBackPressed() }
+
+        addressTextView.text = requireArguments().getString(EXTRA_ADDRESS)
+
+        categoryTextView.setOnClickListener {
+            SelectPlaceCategoryFragment().show(childFragmentManager, "SelectCategoryFragment")
+        }
+    }
+
+    fun withParams(position: LatLng, address: String) = this.apply {
+        arguments = bundleOf(
+            EXTRA_POSITION to position,
+            EXTRA_ADDRESS to address
+        )
+    }
+
+    override fun onCategorySelected(category: Category) {
+        categoryTextView.category = category
+    }
+
+    private companion object {
+        private const val EXTRA_POSITION = "jp.co.soramitsu.map.presentation.places.add.Position"
+        private const val EXTRA_ADDRESS = "jp.co.soramitsu.map.presentation.places.add.Address"
+    }
 }
